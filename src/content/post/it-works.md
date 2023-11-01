12fe381f071eb21a2596bdb485aa6db6d97df525
Quentin Delettre <quentin.delettre@gmail.com>
2023-11-01 16:58:59 +0100

    Initial commit

---
+++ src/content/post/it-works.md
@@ -0,0 +1,8 @@
+---
+title: "It works..."
+description: "git commit -m 'feat: yolo' --no-verify && git push -f"
+publishDate: "01 Nov 2023"
+tags: ["test"]
+---
+
+...on my machine. Maybe on yours too 👏
---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export type SiteConfig = {
+	author: string;
+	title: string;
+	description: string;
+	lang: string;
+	ogLocale: string;
+	date: {
+		locale: string | string[] | undefined;
+		options: Intl.DateTimeFormatOptions;
+	};
+	includeViewTransitions: boolean;
+};
+
+export type PaginationLink = {
+	url: string;
+	text?: string;
+	srLabel?: string;
+};
+
+export type SiteMeta = {
+	title: string;
+	description?: string;
+	ogImage?: string | undefined;
+	articleDate?: string | undefined;
+};
